f10e61a09dae19e146dbb814285b6648bf90fd43
Michael Osoro <osoromichael@gmail.com>
2024-02-29 19:03:14 +0300

    update : added more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { BaseComponent } from './components/base/base.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    BaseComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/base/base.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base',
+  template: `
+    <p>
+      base works!
+    </p>
+  `,
+  styleUrls: ['./base.component.scss']
+})
+export class BaseComponent {
+
+}
