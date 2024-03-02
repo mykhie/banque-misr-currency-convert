10ac22789353b14d765131b9994450f2d0d7bd9b
Michael Osoro <osoromichael@gmail.com>
2024-03-02 17:57:01 +0300

    buggy fix

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -2,7 +2,6 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
-import {By} from "@angular/platform-browser";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
